45eb6dd7753d7d0cd72eccaaaa2f1f105144afa6
madewithlinux <madewithlinux.josh@gmail.com>
2016-04-08 14:32:04 -0500

    parallelization using openmp

--- src/libs/generators.cpp
+++ src/libs/generators.cpp
@@ -77,7 +77,6 @@ namespace image_utils {
                 }
             }
         }
-//        return (*w)(100 * std::sqrt(min_dist) / wave_size + offset);
         return w(100 * std::sqrt(min_dist) / wave_size + offset);
     }
 
@@ -130,8 +129,9 @@ namespace image_utils {
     }
 
     void image_fill_2d_wave(matrix<double> &grid, wave_2d *w_2d) {
-        vect mid(grid.x() / 2.0, grid.y() / 2.0);
-        double mag = std::min(grid.x(), grid.y()) / 2;
+        const vect mid(grid.x() / 2.0, grid.y() / 2.0);
+        const double mag = std::min(grid.x(), grid.y()) / 2;
+#pragma omp parallel for schedule(static) collapse(2)
         for (size_t x = 0; x < grid.x(); x++) {
             for (size_t y = 0; y < grid.y(); y++) {
                 /*current point*/
@@ -151,6 +151,78 @@ namespace image_utils {
             for (size_t y = 0; y < out.y(); y++) {
                 out(x, y) = w(in(x, y) + offset);
             }
+        }
+
+    }
+
+    dist_lissajous::dist_lissajous(const wave &w, const size_t table_size,
+                                   const double wave_size,
+                                   const double A, const double B,
+                                   const double a, const double b,
+                                   const double sigma) : distance_wave(w,
+                                                                       table_size,
+                                                                       wave_size) {
+        max_t = (2.01 * PI);
+        wid = (size_t) (table_size * PI / (12 * max_t));
+
+        lookup_table.reserve(table_size);
+        lookup_table.assign(table_size, cached_value(0, 0, 0, 0, 0, 0, 0));
+
+        cached_value *out = lookup_table.data();
+        size_t max = lookup_table.size();
+#pragma omp parallel for schedule(static)
+        for (size_t i = 0; i < max; i++) {
+            const double t = i * max_t / table_size;
+            lookup_table[i].t = t * 1.0;
+            lookup_table[i].C1_0 = A * A * pow(sin(a * t + sigma), 2) +
+                                   B * B * pow(sin(b * t), 2);
+            lookup_table[i].C1_x1 = -2 * A * sin(a * t + sigma);
+            lookup_table[i].C1_y1 = -2 * B * sin(b * t);
+            lookup_table[i].C2_0 = 2 * A * A * a * cos(a * t + sigma) *
+                                   sin(a * t + sigma) +
+                                   2 * B * B * b * cos(b * t) * sin(b * t);
+            lookup_table[i].C2_x1 = -2 * A * a * cos(a * t + sigma);
+            lookup_table[i].C2_y1 = -2 * B * b * cos(b * t);
+        }
+    }
+
+    rose_dist::rose_dist(const wave &w, const size_t table_size,
+                         const double wav_sz,
+                         const int n, const int d) : distance_wave(w, table_size,
+                                                                   wav_sz) {
+
+        /*rho=1 if n*d is odd, rho=2 if n*d is even*/
+        /*ref: http://www.lmtsd.org/cms/lib/PA01000427/Centricity/Domain/116/Polar%20Roses.pdf*/
+        double rho = ((n * d) % 2) ? 1.0 : 2.0;
+        /*1.01 to account for rounding errors on teh max value*/
+        max_t = PI * d * rho * 1.01;
+        const double k = (double) (n) / (double) (d);
+
+        lookup_table.reserve(table_size);
+        lookup_table.assign(table_size, cached_value(0, 0, 0, 0, 0, 0, 0));
+#pragma omp parallel for schedule(static)
+        for (size_t i = 0; i < lookup_table.size(); i++) {
+            const double t = i * max_t / table_size;
+            lookup_table[i].t = t * 1.0;
+            lookup_table[i].C1_0 = pow(cos(k * t), 2) * pow(cos(t), 2) +
+                                   pow(cos(k * t), 2) * pow(sin(t), 2);
+            lookup_table[i].C1_x1 = -2 * cos(k * t) * cos(t);
+            lookup_table[i].C1_y1 = -2 * cos(k * t) * sin(t);
+            lookup_table[i].C2_0 =
+                    -2 * k * cos(k * t) * pow(cos(t), 2) * sin(k * t) -
+                    2 * k * cos(k * t) * sin(k * t) * pow(sin(t), 2);
+            lookup_table[i].C2_x1 =
+                    2 * k * cos(t) * sin(k * t) + 2 * cos(k * t) * sin(t);
+            lookup_table[i].C2_y1 =
+                    2 * k * sin(k * t) * sin(t) - 2 * cos(k * t) * cos(t);
+        }
+
+        /*determine interval width*/
+        /*PI/(3*max_t) determined by experimentation*/
+        if (n < d) {
+            wid = (size_t) (table_size * PI / (3 * max_t));
+        } else {
+            wid = (size_t) (table_size * PI / (6 * rho * max_t));
         }
 
     }
--- src/libs/generators.h
+++ src/libs/generators.h
@@ -6,7 +6,6 @@
 
 #include <iostream>
 #include "types.h"
-//#include "debug.h"
 
 namespace image_utils {
 
@@ -47,26 +46,21 @@ namespace image_utils {
                          const double &x,
                          const double &y) const;
 
-    protected:
-        /*these values must be provided by the subclass*/
-        double offset;
-        double max_t;
-        size_t wid;
-
+    public:
         struct cached_value {
 
             /*constants needed in general*/
-            const double t;
+            double t;
 
             /*constants needed for distance*/
-            const double C1_0;
-            const double C1_x1;
-            const double C1_y1;
+            double C1_0;
+            double C1_x1;
+            double C1_y1;
 
             /*constants needed for derivative of distance*/
-            const double C2_0;
-            const double C2_x1;
-            const double C2_y1;
+            double C2_0;
+            double C2_x1;
+            double C2_y1;
 
             cached_value(const double _t,
                          const double _C1_0,
@@ -98,6 +92,12 @@ namespace image_utils {
             }
         };
 
+    protected:
+        /*these values must be provided by the subclass*/
+        double offset;
+        double max_t;
+        size_t wid;
+
         std::vector<cached_value> lookup_table;
 
     public:
@@ -110,47 +110,13 @@ namespace image_utils {
                                   const double &y) const;
 
         virtual ~distance_wave();
-
     };
 
     class rose_dist : public distance_wave {
     public:
         rose_dist(const wave &w, const size_t table_size,
                   const double wav_sz,
-                  const int n, const int d) : distance_wave(w, table_size,
-                                                            wav_sz) {
-
-            /*rho=1 if n*d is odd, rho=2 if n*d is even*/
-            /*ref: http://www.lmtsd.org/cms/lib/PA01000427/Centricity/Domain/116/Polar%20Roses.pdf*/
-            double rho = ((n * d) % 2) ? 1.0 : 2.0;
-            /*1.01 to account for rounding errors on teh max value*/
-            max_t = PI * d * rho * 1.01;
-
-            const double k = (double) (n) / (double) (d);
-            for (double t = 0; t <= max_t; t += max_t / table_size) {
-                lookup_table.emplace_back(t * 1.0,
-                                          pow(cos(k * t), 2) * pow(cos(t), 2) +
-                                          pow(cos(k * t), 2) * pow(sin(t), 2),
-                                          -2 * cos(k * t) * cos(t),
-                                          -2 * cos(k * t) * sin(t),
-                                          -2 * k * cos(k * t) * pow(cos(t), 2) *
-                                          sin(k * t) -
-                                          2 * k * cos(k * t) * sin(k * t) *
-                                          pow(sin(t), 2),
-                                          2 * k * cos(t) * sin(k * t) +
-                                          2 * cos(k * t) * sin(t),
-                                          2 * k * sin(k * t) * sin(t) -
-                                          2 * cos(k * t) * cos(t));
-            }
-            /*determine interval width*/
-            /*PI/(3*max_t) determined by experimentation*/
-            if (n < d) {
-                wid = (size_t) (table_size * PI / (3 * max_t));
-            } else {
-                wid = (size_t) (table_size * PI / (6 * rho * max_t));
-            }
-
-        }
+                  const int n, const int d);
     };
 
     class dist_lissajous : public distance_wave {
@@ -159,24 +125,7 @@ namespace image_utils {
                        const double wave_size,
                        const double A, const double B,
                        const double a, const double b,
-                       const double sigma) : distance_wave(w, table_size,
-                                                           wave_size) {
-            max_t = (2.01 * PI);
-            wid = (size_t) (table_size * PI / (12 * max_t));
-            for (double t = 0; t <= max_t; t += max_t / table_size) {
-                lookup_table.emplace_back(t * 1.0,
-                                          A * A * pow(sin(a * t + sigma), 2) +
-                                          B * B * pow(sin(b * t), 2),
-                                          -2 * A * sin(a * t + sigma),
-                                          -2 * B * sin(b * t),
-                                          2 * A * A * a * cos(a * t + sigma) *
-                                          sin(a * t + sigma)
-                                          + 2 * B * B * b * cos(b * t) *
-                                            sin(b * t),
-                                          -2 * A * a * cos(a * t + sigma),
-                                          -2 * B * b * cos(b * t));
-            }
-        }
+                       const double sigma);
     };
 
 

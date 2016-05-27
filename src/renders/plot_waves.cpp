f3adefede7f9d4bbe1c5aa821936468c0116e0bd
madewithlinux <madewithlinux.josh@gmail.com>
2016-05-27 17:31:38 -0500

    fixed plot_waves with new wave types

--- src/renders/plot_waves.cpp
+++ src/renders/plot_waves.cpp
@@ -9,22 +9,28 @@
 int main(int argc, char const *argv[]) {
     using namespace image_utils;
 
-    std::map<std::string, wave*> waves_with_labels;
-    waves_with_labels["triangle"] = new wave_triangle();
-    waves_with_labels["square"] = new wave_square();
-    waves_with_labels["sine"] = new wave_sine();
-    waves_with_labels["sawtooth"] = new wave_sawtooth();
-    waves_with_labels["fourier n=2"] = new wave_fourier_square(2);
-    waves_with_labels["fourier n=3"] = new wave_fourier_square(3);
-    waves_with_labels["fourier n=4"] = new wave_fourier_square(4);
-    waves_with_labels["fourier n=5"] = new wave_fourier_square(5);
-    waves_with_labels["fourier n=7"] = new wave_fourier_square(7);
+    std::map<std::string, wave> waves_with_labels;
+//    waves_with_labels["triangle"] = wave(std::string("triangle"));
+    waves_with_labels.insert(std::make_pair("triangle", wave("triangle")));
+    waves_with_labels.insert(std::make_pair("square", wave("square")));
+    waves_with_labels.insert(std::make_pair("sine", wave("sine")));
+    waves_with_labels.insert(std::make_pair("sawtooth", wave("sawtooth")));
+    waves_with_labels.insert(
+            std::make_pair("fourier n=2", wave("fourier_square:2")));
+    waves_with_labels.insert(
+            std::make_pair("fourier n=3", wave("fourier_square:3")));
+    waves_with_labels.insert(
+            std::make_pair("fourier n=4", wave("fourier_square:4")));
+    waves_with_labels.insert(
+            std::make_pair("fourier n=5", wave("fourier_square:5")));
+    waves_with_labels.insert(
+            std::make_pair("fourier n=7", wave("fourier_square:7")));
 
     std::cout << "clear;clc;close all;" << std::endl;
     std::cout << "x = [" << std::endl;
     std::vector<double> xvalues;
-    for (double x = 0; x <= 2; x += 2.0L/10000.0L) {
-        std::cout << std::setprecision(7) << x << ", " ;
+    for (double x = 0; x <= 2; x += 2.0L / 10000.0L) {
+        std::cout << std::setprecision(7) << x << ", ";
         xvalues.push_back(x);
     }
     std::cout << "];" << std::endl;
@@ -32,20 +38,20 @@ int main(int argc, char const *argv[]) {
     std::cout << "waves = [" << std::endl;
 
     std::vector<std::string> labels;
-    for (auto &g : waves_with_labels ) {
+    for (auto &g : waves_with_labels) {
         std::string label = g.first;
         labels.push_back(label);
-        wave *w = g.second;
+        wave &w = g.second;
         for (auto x : xvalues) {
-            std::cout << std::setprecision(7) << (*w)(x) << ", " ;
+            std::cout << std::setprecision(7) << (w)(x) << ", ";
         }
         std::cout << ";" << std::endl;
     }
     std::cout << "];" << std::endl;
 
     std::cout << "plot(" << std::endl;
-    for (int i=0; i<labels.size()-1; i++) {
-        std::cout << "x, waves(" << i+1 << ",:)," << std::endl;
+    for (int i = 0; i < labels.size() - 1; i++) {
+        std::cout << "x, waves(" << i + 1 << ",:)," << std::endl;
     }
     std::cout << "x, waves(" << labels.size() << ",:)" << std::endl;
     std::cout << ")" << std::endl;

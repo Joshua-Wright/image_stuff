d9c5dd64c0651d7b5c359b172061ed715d320a9d
Josh Wright <madewithlinux.josh@gmail.com>
2017-12-21 15:51:55 -0600

    sierpinski arrowhead smoothing animation, and also dragon curve

--- go/main/dragon_curve.go
+++ go/main/dragon_curve.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	m "github.com/joshua-wright/image_stuff/go"
-	"math"
 )
 
 func main() {
@@ -20,21 +19,37 @@ func main() {
 	//	m.Vec2{0, -1}, m.Vec2{0, 0}, m.Vec2{1, 0},
 	//)
 
-	r := 1.0 / math.Sqrt2
-	theta1 := math.Pi / 4
-	theta2 := 5 * math.Pi / 4
-	t1 := m.Matrix3{
-		{r * math.Cos(theta1), -r * math.Sin(theta1), 0},
-		{r * math.Sin(theta1), r * math.Cos(theta1), 0},
-		{0, 0, 1},
-	}
-	t2 := m.Matrix3{
-		{r * math.Cos(theta2), -r * math.Sin(theta2), 1},
-		{r * math.Sin(theta2), r * math.Cos(theta2), 0},
-		{0, 0, 1},
-	}
+	//r := 1.0 / math.Sqrt2
+	//theta1 := math.Pi / 4
+	//theta2 := 5 * math.Pi / 4
+	//t1 := m.Matrix3{
+	//	{r * math.Cos(theta1), -r * math.Sin(theta1), 0},
+	//	{r * math.Sin(theta1), r * math.Cos(theta1), 0},
+	//	{0, 0, 1},
+	//}
+	//t2 := m.Matrix3{
+	//	{r * math.Cos(theta2), -r * math.Sin(theta2), 1},
+	//	{r * math.Sin(theta2), r * math.Cos(theta2), 0},
+	//	{0, 0, 1},
+	//}
+	//
+	//mats := []m.Matrix3{t1, t2}
 
-	mats := []m.Matrix3{t1, t2}
+	a := m.Vec2{-1, 0}
+	b := m.Vec2{0, -1}
+	c := m.Vec2{1, 0}
+	d := m.Vec2{-1, -1}
+	e := m.Vec2{0, 0}
+	mats := []m.Matrix3{
+		m.MatrixImage2D(
+			a, b, c,
+			a, d, b,
+		),
+		m.MatrixImage2D(
+			a, b, c,
+			c, e, b,
+		),
+	}
 
 	width := 1000
 	xmid := 0.0
--- go/main/sierpinski_arrowhead_smoothing.go
+++ go/main/sierpinski_arrowhead_smoothing.go
@@ -5,7 +5,8 @@ import (
 	"os"
 	"fmt"
 	"sync"
-	"image/png"
+	"image"
+	"image/gif"
 )
 
 func main() {
@@ -49,7 +50,7 @@ func main() {
 	os.Mkdir(folder_path, 0777)
 
 	n_frames := 150
-	//frames := make([]*image.Paletted, n_frames)
+	frames := make([]*image.Paletted, n_frames)
 
 	var wg sync.WaitGroup
 	for i := 0; i < n_frames; i++ {
@@ -58,29 +59,30 @@ func main() {
 			fmt.Println("smoothness:", smoothness)
 			pts2 := m.BSplineAdaptive(pts, smoothness, 2/m.Float(width))
 
-			//img := m.RasterizePointsPalletted(width, pts2, bounds)
-			//frames[smoothness] = img
+			img := m.RasterizePointsPalletted(width, pts2, bounds)
+			frames[smoothness] = img
 
-			img := m.RasterizePoints0(width, pts2, bounds)
-			file, err := os.Create(fmt.Sprintf("%s/%s_%d.png", folder_path, folder_path, smoothness))
-			m.Die(err)
-			m.Die(png.Encode(file, img))
-			m.Die(file.Close())
+			//img := m.RasterizePoints0(width, pts2, bounds)
+			//file, err := os.Create(fmt.Sprintf("%s/%s_%d.png", folder_path, folder_path, smoothness))
+			//m.Die(err)
+			//m.Die(png.Encode(file, img))
+			//m.Die(file.Close())
+
 			wg.Done()
 		}(i)
 	}
 	wg.Wait()
 
-	//outGif := &gif.GIF{}
-	//outGif.Image = frames
-	//outGif.Delay = make([]int, n_frames)
-	//for i := 0; i < n_frames; i++ {
-	//	outGif.Delay[i] = 0
-	//}
-	//
-	//file, err := os.OpenFile(m.ExecutableNameWithExtension("gif"), os.O_WRONLY|os.O_CREATE, 0600)
-	//m.Die(err)
-	//defer file.Close()
-	//gif.EncodeAll(file, outGif)
+	outGif := &gif.GIF{}
+	outGif.Image = frames
+	outGif.Delay = make([]int, n_frames)
+	for i := 0; i < n_frames; i++ {
+		outGif.Delay[i] = 0
+	}
+
+	file, err := os.OpenFile(m.ExecutableNameWithExtension("gif"), os.O_WRONLY|os.O_CREATE, 0600)
+	m.Die(err)
+	defer file.Close()
+	gif.EncodeAll(file, outGif)
 
 }
